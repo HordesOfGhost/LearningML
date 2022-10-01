4f2242786a53d150cb52d62864ceaea680a14498
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-10-01 10:20:19 +0545

    Covariance and Correlation Complete

--- StatsBasic/Covariance and Correlation.py
+++ StatsBasic/Covariance and Correlation.py
@@ -1,5 +1,7 @@
 #%%
+#random data
 import numpy as np
+import matplotlib.pyplot as plt
 
 def de_mean(x):
     xmean=np.mean(x)
@@ -7,7 +9,55 @@ def de_mean(x):
 
 def covariance(x,y):
     n=len(x)
-    return np.dot((de_mean(x),de_mean(y))/(n-1))
+    return np.dot(de_mean(x),de_mean(y))/(n-1)
+
+def correlation(x,y):
+    stdofx=x.std()
+    stdofy=y.std()
+    return covariance(x,y)/stdofx/stdofy
 
 pageSpeeds=np.random.normal(3,1,1000)
 purchaseAmount=np.random.normal(50,10,1000)
+
+plt.scatter(pageSpeeds,purchaseAmount)
+print(covariance(pageSpeeds,purchaseAmount))
+print (correlation(pageSpeeds,purchaseAmount))
+print(np.corrcoef(pageSpeeds,purchaseAmount))
+
+
+# %%
+# negative covariance
+purchaseAmount=np.random.normal(50,10,1000)/pageSpeeds
+plt.scatter(pageSpeeds,purchaseAmount)
+print(covariance(pageSpeeds,purchaseAmount))
+print (correlation(pageSpeeds,purchaseAmount))
+print(np.corrcoef(pageSpeeds,purchaseAmount))
+
+# %%
+#positive maybe
+purchaseAmount=np.random.normal(50,10,1000)*pageSpeeds
+plt.scatter(pageSpeeds,purchaseAmount)
+print(covariance(pageSpeeds,purchaseAmount))
+print (correlation(pageSpeeds,purchaseAmount))
+print(np.corrcoef(pageSpeeds,purchaseAmount))
+# %%
+
+print (correlation(pageSpeeds,purchaseAmount))
+
+# %%
+##   NUMPY WAY
+
+print(np.corrcoef(pageSpeeds,purchaseAmount))
+# %%
+# Forcing Correlation
+purchaseAmount=100-pageSpeeds*3
+plt.scatter(pageSpeeds,purchaseAmount)
+print(correlation(pageSpeeds,purchaseAmount))
+# %%
+purchaseAmount=pageSpeeds*3+100
+plt.scatter(pageSpeeds,purchaseAmount)
+print(correlation(pageSpeeds,purchaseAmount))
+# %%
+purchaseAmount=pageSpeeds*9
+print(np.cov(pageSpeeds,purchaseAmount))
+# %%
